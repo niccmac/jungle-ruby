998253e10cfca8d545838653fefbd66c8536070f
Nicole MacLean <nicole.mac0404@gmail.com>
2022-05-04 13:05:33 -0700

    Admin dashboard table displays product and cat count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @count_products = Product.count
+    @count_cats = Category.count
   end
 end

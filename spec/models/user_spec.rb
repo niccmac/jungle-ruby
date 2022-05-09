cc351cf022fda14db79dbb369a93d0f5fe1d6932
Nicole MacLean <nicole.mac0404@gmail.com>
2022-05-09 16:32:51 -0700

    Add 2nd user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validation" do
 
-    it "Created with password and password_confirmation" do
+    it "Created with correct details" do
       user = User.create first_name: "Fake Name", last_name: "Fake Last Name", email: "test@test.com", password: "123", password_confirmation: "123"
       expect(user).to be_valid
     end
-    
+
+    it "Created without matching password and password_confirmation" do
+      user = User.create first_name: "Fake Name", last_name: "Fake Last Name", email: "test@test.com", password: "123", password_confirmation: "124"
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
   end 
 end
